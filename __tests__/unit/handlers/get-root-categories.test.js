1ecb26a91da705074bd091775bc81163b2ee8edd
EC2 Default User <ec2-user@ip-172-31-0-129.eu-west-1.compute.internal>
2020-11-15 10:28:21 +0000

    get root categories test added

--- __tests__/unit/handlers/get-root-categories.test.js
+++ __tests__/unit/handlers/get-root-categories.test.js
@@ -1,16 +1,21 @@
 // Import all functions from hello-from-lambda.js
-const lambda = require('../../../src/handlers/get-root-categories.js');
+const lambda = require('../../../src/handlers/hello-from-lambda.js');
 
-describe('Test get-root-categories', () => {
+describe('Test for hello-from-lambda', () => {
     // This test invokes the hello-from-lambda Lambda function and compares the result
-    it('Returrns an array of categories', async () => {
+    it('Verifies successful response', async () => {
         // Invoke helloFromLambdaHandler
-        const result = await lambda.getRootCategoriesHandler();
+        const result = await lambda.helloFromLambdaHandler();
 
         /*
             The expected result should match the return from your Lambda function.
+            e.g.
+                If you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js,
+                you should change the following line to `const expectedResult = 'Hello World!';`
         */
+        const expectedResult = 'Hello from Lambda!';
+
         // Compare the result with the expected result
-        expect(result).to.be.array();
+        expect(result).toEqual(expectedResult);
     });
 });
